0a91f2de33e150074020671abe99e8d3917d1856
Brian Takita <brian.takita@gmail.com>
2022-01-09 02:10:14 -0600

    ## 16.0.11
    ### Patch Changes
    
    - fix: search*result_store*: infinite recursion

--- src/search_result_store_.ts
+++ src/search_result_store_.ts
@@ -1,44 +1,48 @@
+import { run } from '@ctx-core/function'
 import { setter_computed$, ReadableAtom$ } from '@ctx-core/nanostores'
 export function search_result_store_<I extends unknown = unknown, O extends unknown = unknown>(
 	{
 		query$, data_, clear
 	}:search_result_opts_I<I, O>
 ):search_result$_T<I, O> {
+	let current_search_store:search_result_T<I, O>
 	const search_store$ = setter_computed$<search_result_T<I, O>, ReadableAtom$<I>>(
 		query$,
-		(query:I, set)=>{
+		(query:I, _set)=>{
 			// const set = in_set as (value:search_result_T)=>void
 			if (!query) {
 				(clear || (()=>{
-					set(
-						{ done: true, loading: false, query$, data: [] } as search_result_T<I, O>
-					)
+					set(_set, {
+						done: true, loading: false, query$, data: []
+					} as search_result_T<I, O>)
 				}))()
 				return
 			}
-			const previous_search:search_result_T<I, O> = search_store$.$
-			const previous_query = previous_search && previous_search.query$
-			if (previous_query === query) {
+			if (current_search_store?.query$ === query) {
 				return
 			}
-			set({
+			set(_set, {
 				done: false,
 				loading: true,
 				query$,
 			} as search_result_T<I, O>)
-			;(async ()=>{
+			run(async ()=>{
 				const data = await data_({ query })
 				if (query === query$.$) {
-					set({
+					set(_set, {
 						done: true,
 						loading: false,
 						query$,
 						data,
 					} as search_result_T<I, O>)
 				}
-			})()
+			}).then()
 		})
 	return search_store$
+	function set(_set:(newValue:search_result_T<I, O>)=>void, search_store:search_result_T<I, O>) {
+		current_search_store = search_store
+		_set(search_store)
+	}
 }
 export interface search_result_opts_I<I extends unknown = unknown, O extends unknown = unknown> {
 	query$:ReadableAtom$<I>
